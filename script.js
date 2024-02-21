20362c390a418ec7aa6cb449bf19bc1cb9c246af
Juliana <jullibarbosa.sandes@gmail.com>
2024-02-21 16:52:46 -0300

    Alteração no tamanho foto, hover button list e no script

--- script.js
+++ script.js
@@ -12,15 +12,14 @@ function toggleMode() {
 
   //pegar a tag img
   const img = document.querySelector("#profile img")
-  const desq = document.querySelector("alt")
 
   //substituir a img
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
-    desq.setAttribute("alt", "Foto de Juliana Sandes usando óculos no modo claro")
+  
   } else {
     img.setAttribute("src", "./assets/avatar.png")
-    desq.setAttribute("alt", "Foto de Juliana Sandes usando óculos no modo escuro")
+    
   }
 
 }
